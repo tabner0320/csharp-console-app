95e93bc07c5111270ad85fd5b46324036c9919e3
Theophilus (Theo) Abner <theophilusabner0204@yahoo.com>
2026-03-18 16:28:47 -0400

    Initial commit - Theo Console app assignment

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello World!");
+Console.WriteLine("My Name is Theo");
+Console.WriteLine("What's your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Nice to meet you, {name}!");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(); // This WILL work in VS Code
